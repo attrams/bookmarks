3be7082f5cf68bfe0c22d8d1dbf37c4d60a31e78
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-08 20:37:35 +0000

    add email configuration

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'account:dashboard'
 LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
+
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
