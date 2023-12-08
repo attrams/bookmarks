39a600d1fc5d94fab5a342b4da13bdc91ec12a5e
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-08 21:05:32 +0000

    add user registration form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+
+# this class is not been used as it was being used by the user_login()
+# class LoginForm(forms.Form):
+#     username = forms.CharField()
+#     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Password don\'t match.')
+        return cd['password2']
