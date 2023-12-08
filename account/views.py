57e221bd5275f44e1b34f86364ebb88784d6c947
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-08 19:24:12 +0000

    comment out user_login()

--- account/views.py
+++ account/views.py
@@ -10,30 +10,30 @@ from .forms import LoginForm
 # this function logs user in but it currently not being used since django authentication is being used.
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
 
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request=request,
-                username=cd['username'],
-                password=cd['password'],
-            )
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(
+#                 request=request,
+#                 username=cd['username'],
+#                 password=cd['password'],
+#             )
 
-            if user is not None:
-                if user.is_active:
-                    login(request=request, user=user)
-                    return HttpResponse(content='Authenticated successfully')
-                else:
-                    return HttpResponse(content='Disabled account')
-            else:
-                return HttpResponse(content='Invalid login')
-    else:
-        form = LoginForm()
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request=request, user=user)
+#                     return HttpResponse(content='Authenticated successfully')
+#                 else:
+#                     return HttpResponse(content='Disabled account')
+#             else:
+#                 return HttpResponse(content='Invalid login')
+#     else:
+#         form = LoginForm()
 
-    return render(request=request, template_name='account/login.html', context={'form': form})
+#     return render(request=request, template_name='account/login.html', context={'form': form})
 
 
 @login_required
