ebe8ec6be2239e3079c11af38358beeef5958ce4
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-14 20:16:49 +0000

    return url in clean_url()

--- images/forms.py
+++ images/forms.py
@@ -1,4 +1,3 @@
-from distutils import extension
 from django import forms
 from .models import Image
 from django.core.files.base import ContentFile
@@ -22,6 +21,8 @@ class ImageCreateForm(forms.ModelForm):
         if extension not in valid_extensions:
             raise forms.ValidationError(
                 'The given URL does not match valid image extensions.')
+
+        return url
 
     def save(self, force_insert=False, force_update=False, commit=True):
         image = super().save(commit=False)
