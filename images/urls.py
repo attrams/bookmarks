4521be640c53d2d1e9cf00b56460fa3e88c48b5a
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-17 21:11:12 +0000

    add image_like() view and url

--- images/urls.py
+++ images/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path(route='create/', view=views.image_create, name='create'),
     path(route='detail/<int:id>/<slug:slug>/',
          view=views.image_detail, name='detail'),
+    path(route='like/', view=views.image_like, name='like')
 ]
