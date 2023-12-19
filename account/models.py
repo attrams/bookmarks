778160e109fa9c6beb5935d2a2984ec90440e6a4
attrams <48452451+Attrams@users.noreply.github.com>
2023-12-19 07:30:08 +0000

    add Contact model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
@@ -12,3 +12,26 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"Profile of {self.user.username}"
+
+
+class Contact(models.Model):
+    '''
+    This model serves as the relationship model(follower and following).
+    '''
+    user_from = models.ForeignKey(
+        'auth.User', related_name='rel_from_set', on_delete=models.CASCADE)
+    user_to = models.ForeignKey(
+        'auth.User', related_name='rel_to_set', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['-created'])]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f"{self.user_from} follows {self.user_to}"
+
+
+user_model = get_user_model()
+user_model.add_to_class(name='following', value=models.ManyToManyField(
+    'self', through=Contact, related_name='followers', symmetrical=False))
